1c66bc0344b92835a7898fe3d70d379c327c8779
RideWindX <ridewindx@163.com>
2017-03-06 17:18:11 +0800

    Update material.go

--- public/material.go
+++ public/material.go
@@ -1,9 +1,5 @@
 package public
 
-import (
-	"mime/multipart"
-)
-
 const (
 	Image = "image"
 	Voice = "voice"
@@ -12,10 +8,48 @@ const (
 	News  = "news"
 )
 
-type MediaInfo struct {
-	MediaType string `json:"type"`
-	MediaId   string `json:"media_id"`
+type Media struct {
+	Type      string `json:"type"`
+	Id        string `json:"media_id"`
 	CreatedAt int64  `json:"created_at"`
 }
 
+func (c *client) UploadImage(filePath string) (*Media, error) {
+	return c.UploadMedia(Image, filePath)
+}
 
+func (c *client) UploadVoice(filePath string) (*Media, error) {
+	return c.UploadMedia(Voice, filePath)
+}
+
+func (c *client) UploadVideo(filePath string) (*Media, error) {
+	return c.UploadMedia(Video, filePath)
+}
+
+func (c *client) UploadThumb(filePath string) (*Media, error) {
+	return c.UploadMedia(Thumb, filePath)
+}
+
+func (c *client) UploadMedia(mediaType, filePath string) (*Media, error) {
+	u := BASE_URL.Join("/media/upload").Query("type", mediaType)
+
+	var rep struct {
+		Err
+		Media
+	}
+
+	err := c.UploadFile(u, "media", filePath, &rep)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rep.Media, err
+}
+
+func (c *client) DownloadMedia(mediaId, filePath string) error {
+	u := BASE_URL.Join("/media/get").Query("media_id", mediaId) // TODO: download video needs http, not https
+
+	var rep Err
+
+	return c.DownloadFile(u, filePath, &rep)
+}
