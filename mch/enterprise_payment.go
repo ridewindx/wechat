420a3db6cec7fab086f801030aa994a31baf4520
RideWindX <ridewindx@163.com>
2017-11-30 13:05:19 +0800

    Update files

--- mch/enterprise_payment.go
+++ mch/enterprise_payment.go
@@ -4,32 +4,32 @@ import "strconv"
 
 type EnterprisePayRequst struct {
 	// 必选参数
-	OutTradeNo     string // 商户订单号，需保持唯一性(只能是字母或者数字，不能包含有符号)
-	Amount int // 企业付款金额，单位为分
-	Desc string // 企业付款操作说明信息
-	FromIP string // 调用接口的机器Ip地址
-	OpenID string // 商户appid下，某用户的openid
-	IsCheckName bool // 是否校验真实姓名
+	OutTradeNo  string // 商户订单号，需保持唯一性(只能是字母或者数字，不能包含有符号)
+	Amount      int    // 企业付款金额，单位为分
+	Desc        string // 企业付款操作说明信息
+	FromIP      string // 调用接口的机器Ip地址
+	OpenID      string // 商户appid下，某用户的openid
+	IsCheckName bool   // 是否校验真实姓名
 
 	// 可选参数
-	UserName string // 收款用户真实姓名，如果check_name设置为FORCE_CHECK，则必填用户真实姓名
+	UserName   string // 收款用户真实姓名，如果check_name设置为FORCE_CHECK，则必填用户真实姓名
 	DeviceInfo string // 微信支付分配的终端设备号
 }
 
 type EnterprisePayResponse struct {
-	PaymentNo string // 企业付款成功，返回的微信订单号
+	PaymentNo   string // 企业付款成功，返回的微信订单号
 	PaymentTime string // 企业付款成功时间
-	OutTradeNo     string // 商户订单号
-	DeviceInfo string // 调用接口提交的终端设备号
+	OutTradeNo  string // 商户订单号
+	DeviceInfo  string // 调用接口提交的终端设备号
 }
 
 func (client *Client) EnterprisePay(req *EnterprisePayRequst) (*EnterprisePayResponse, error) {
 	reqMap := map[string]string{
 		"partner_trade_no": req.OutTradeNo,
-		"amount": strconv.Itoa(req.Amount),
-		"desc": req.Desc,
+		"amount":           strconv.Itoa(req.Amount),
+		"desc":             req.Desc,
 		"spbill_create_ip": req.FromIP,
-		"openid": req.OpenID,
+		"openid":           req.OpenID,
 	}
 
 	if req.IsCheckName {
@@ -48,29 +48,29 @@ func (client *Client) EnterprisePay(req *EnterprisePayRequst) (*EnterprisePayRes
 	}
 
 	rep := &EnterprisePayResponse{
-		PaymentNo: repMap["payment_no"],
+		PaymentNo:   repMap["payment_no"],
 		PaymentTime: repMap["payment_time"],
-		OutTradeNo: repMap["partner_trade_no"],
-		DeviceInfo: repMap["device_info"],
+		OutTradeNo:  repMap["partner_trade_no"],
+		DeviceInfo:  repMap["device_info"],
 	}
 	return rep, nil
 }
 
 const (
-	EnterprisePaymentSuccess = "SUCCESS"
-	EnterprisePaymentFailed = "FAILED"
+	EnterprisePaymentSuccess    = "SUCCESS"
+	EnterprisePaymentFailed     = "FAILED"
 	EnterprisePaymentProcessing = "PROCESSING"
 )
 
 type QueryEnterprisePaymentResponse struct {
-	Status string // 转账状态
-	Reason string // 如果失败则有失败原因
-	PaymentNo string // 微信订单号
-	Amount int // 付款金额单位分
-	Desc string // 付款时候的描述
+	Status      string // 转账状态
+	Reason      string // 如果失败则有失败原因
+	PaymentNo   string // 微信订单号
+	Amount      int    // 付款金额单位分
+	Desc        string // 付款时候的描述
 	PaymentTime string // 发起转账的时间
-	OpenID string // 商户appid下用户的openid
-	UserName string // 收款用户真实姓名
+	OpenID      string // 商户appid下用户的openid
+	UserName    string // 收款用户真实姓名
 }
 
 func (client *Client) QueryEnterprisePayment(outTradeNo string) (*QueryEnterprisePaymentResponse, error) {
@@ -86,14 +86,14 @@ func (client *Client) QueryEnterprisePayment(outTradeNo string) (*QueryEnterpris
 		return nil, err
 	}
 	rep := &QueryEnterprisePaymentResponse{
-		Status: repMap["status"],
-		Reason: repMap["reason"],
-		PaymentNo: repMap["detail_id"],
-		Amount: amount,
-		Desc: repMap["desc"],
+		Status:      repMap["status"],
+		Reason:      repMap["reason"],
+		PaymentNo:   repMap["detail_id"],
+		Amount:      amount,
+		Desc:        repMap["desc"],
 		PaymentTime: repMap["transfer_time"],
-		OpenID: repMap["openid"],
-		UserName: repMap["transfer_name"],
+		OpenID:      repMap["openid"],
+		UserName:    repMap["transfer_name"],
 	}
 	return rep, nil
 }
--- mch/error.go
+++ mch/error.go
@@ -36,39 +36,40 @@ func (e *BizError) Error() string {
 const (
 	ErrCodeSYSTEMERROR = "SYSTEMERROR" // 系统异常，请用相同参数重新调用
 
-	ErrCodeORDERPAID = "ORDERPAID" // 订单已支付，请当作已支付的正常交易
-	ErrCodeORDERCLOSED = "ORDERCLOSED" // 订单已关闭
-	ErrCodeNOTENOUGH = "NOTENOUGH" // 用户帐号余额不足，请用户充值或更换支付卡后再支付
+	ErrCodeORDERPAID     = "ORDERPAID"     // 订单已支付，请当作已支付的正常交易
+	ErrCodeORDERCLOSED   = "ORDERCLOSED"   // 订单已关闭
+	ErrCodeNOTENOUGH     = "NOTENOUGH"     // 用户帐号余额不足，请用户充值或更换支付卡后再支付
 	ErrCodeORDERNOTEXIST = "ORDERNOTEXIST" // 查询系统中不存在此交易订单号
 
-	ErrCodeBIZERR_NEED_RETRY = "BIZERR_NEED_RETRY" // 并发情况下业务被拒绝，商户重试即可解决	号，请不要更换商户退款单号
-	ErrCodeERROR = "ERROR" // 申请退款业务发生错误
+	ErrCodeBIZERR_NEED_RETRY     = "BIZERR_NEED_RETRY"     // 并发情况下业务被拒绝，商户重试即可解决	号，请不要更换商户退款单号
+	ErrCodeERROR                 = "ERROR"                 // 申请退款业务发生错误
 	ErrCodeUSER_ACCOUNT_ABNORMAL = "USER_ACCOUNT_ABNORMAL" // 退款请求失败，用户帐号注销
-	ErrCodeINVALID_REQ_TOO_MUCH = "INVALID_REQ_TOO_MUCH" // 连续错误请求数过多被系统短暂屏蔽，请在1分钟后再来重试
-	ErrCodeFREQUENCY_LIMITED = "FREQUENCY_LIMITED" // 2个月之前的订单申请退款有频率限制，请降低频率后重试
-	ErrCodeREFUNDNOTEXIST = "REFUNDNOTEXIST" // 请检查订单号是否有误以及订单状态是否正确，如：未支付、已支付未退款
+	ErrCodeINVALID_REQ_TOO_MUCH  = "INVALID_REQ_TOO_MUCH"  // 连续错误请求数过多被系统短暂屏蔽，请在1分钟后再来重试
+	ErrCodeFREQUENCY_LIMITED     = "FREQUENCY_LIMITED"     // 2个月之前的订单申请退款有频率限制，请降低频率后重试
+	ErrCodeREFUNDNOTEXIST        = "REFUNDNOTEXIST"        // 请检查订单号是否有误以及订单状态是否正确，如：未支付、已支付未退款
 
-	ErrCodeNOAUTH = "NOAUTH" // 商户未开通此接口权限	请商户前往申请此接口权限
-	ErrCodeAPPID_NOT_EXIST = "APPID_NOT_EXIST"
-	ErrCodeMCHID_NOT_EXIST = "MCHID_NOT_EXIST"
+	ErrCodeNOAUTH                = "NOAUTH" // 商户未开通此接口权限	请商户前往申请此接口权限
+	ErrCodeAPPID_NOT_EXIST       = "APPID_NOT_EXIST"
+	ErrCodeMCHID_NOT_EXIST       = "MCHID_NOT_EXIST"
 	ErrCodeAPPID_MCHID_NOT_MATCH = "APPID_MCHID_NOT_MATCH"
-	ErrCodeLACK_PARAMS = "LACK_PARAMS"
-	ErrCodeOUT_TRADE_NO_USED = "OUT_TRADE_NO_USED" // 商户订单号重复，同一笔交易不能多次提交，请核实商户订单号是否重复提交
-	ErrCodeSIGNERROR = "SIGNERROR"
-	ErrCodeXML_FORMAT_ERROR = "XML_FORMAT_ERROR"
-	ErrCodeREQUIRE_POST_METHOD = "REQUIRE_POST_METHOD"
-	ErrCodePOST_DATA_EMPTY = "POST_DATA_EMPTY"
-	ErrCodeNOT_UTF8 = "NOT_UTF8"
-	ErrCodeTRADE_OVERDUE = "TRADE_OVERDUE" // 订单已经超过可退款的最大期限(支付后一年内可退款)
+	ErrCodeLACK_PARAMS           = "LACK_PARAMS"
+	ErrCodeOUT_TRADE_NO_USED     = "OUT_TRADE_NO_USED" // 商户订单号重复，同一笔交易不能多次提交，请核实商户订单号是否重复提交
+	ErrCodeSIGNERROR             = "SIGNERROR"
+	ErrCodeXML_FORMAT_ERROR      = "XML_FORMAT_ERROR"
+	ErrCodeREQUIRE_POST_METHOD   = "REQUIRE_POST_METHOD"
+	ErrCodePOST_DATA_EMPTY       = "POST_DATA_EMPTY"
+	ErrCodeNOT_UTF8              = "NOT_UTF8"
+	ErrCodeTRADE_OVERDUE         = "TRADE_OVERDUE"         // 订单已经超过可退款的最大期限(支付后一年内可退款)
 	ErrCodeINVALID_TRANSACTIONID = "INVALID_TRANSACTIONID" // 检查原交易号是否存在或发起支付交易接口返回失败
-	ErrCodePARAM_ERROR = "PARAM_ERROR"
+	ErrCodePARAM_ERROR           = "PARAM_ERROR"
 
-	ErrCodeNO_AUTH = "NO_AUTH" // 没有授权请求此api
-	ErrCodeAMOUNT_LIMIT = "AMOUNT_LIMIT" // 付款金额不能小于最低限额	每次付款金额必须大于1元
-	ErrCodeOPENID_ERROR = "OPENID_ERROR" // Openid格式错误或者不属于商家公众账号
-	ErrCodeSEND_FAILED = "SEND_FAILED" // 付款失败，请换单号重试
-	ErrCodeNAME_MISMATCH = "NAME_MISMATCH" // 请求参数里填写了需要检验姓名，但是输入了错误的姓名
-	ErrCodeFREQ_LIMIT = "FREQ_LIMIT" // 接口请求频率超时接口限制
-	ErrCodeMONEY_LIMIT = "MONEY_LIMIT" // 已经达到今日付款总额上限/已达到付款给此用户额度上限
+	ErrCodeNO_AUTH               = "NO_AUTH"               // 没有授权请求此api
+	ErrCodeAMOUNT_LIMIT          = "AMOUNT_LIMIT"          // 付款金额不能小于最低限额	每次付款金额必须大于1元
+	ErrCodeOPENID_ERROR          = "OPENID_ERROR"          // Openid格式错误或者不属于商家公众账号
+	ErrCodeSEND_FAILED           = "SEND_FAILED"           // 付款失败，请换单号重试
+	ErrCodeNAME_MISMATCH         = "NAME_MISMATCH"         // 请求参数里填写了需要检验姓名，但是输入了错误的姓名
+	ErrCodeFREQ_LIMIT            = "FREQ_LIMIT"            // 接口请求频率超时接口限制
+	ErrCodeMONEY_LIMIT           = "MONEY_LIMIT"           // 已经达到今日付款总额上限/已达到付款给此用户额度上限
 	ErrCodeV2_ACCOUNT_SIMPLE_BAN = "V2_ACCOUNT_SIMPLE_BAN" // 无法给非实名用户付款
+	ErrCodeNOT_FOUND             = "NOT_FOUND"
 )
